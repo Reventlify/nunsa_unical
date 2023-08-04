04b47f01cede5fa91c588ca6305248cdcc1152f2
Edidiong Obodom <edijay17@gmail.com>
2023-08-04 18:00:20 +0100

    modified:   src/components/createPost/screenSizes/createPostSmall.js

--- src/components/createPost/screenSizes/createPostSmall.js
+++ src/components/createPost/screenSizes/createPostSmall.js
@@ -4,7 +4,7 @@ import classes from "../createPostMain.module.css";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const CreatePostSmall = () => {
-  const [opinion, setOpinion] = useState("What's on your mind?");
+  const [opinion, setOpinion] = useState("");
   const hiddenFileInput = useRef();
   // Programatically click the hidden file input element
   // when the defaultInputText component is clicked
@@ -23,12 +23,16 @@ const CreatePostSmall = () => {
             <div className={`${classes.head} container`}>
               <div className={`${classes.left}`}>
                 <ArrowBackIcon />
-                &nbsp;Create Post
+                &nbsp;<span className="bolder">Create Post</span>
               </div>
               <div className={`${classes.right}`}>
                 <button
-                  className="btn bottomShadow btnct btnct-nunsa"
-                  disabled={false}
+                  className={
+                    opinion.length > 0
+                      ? "btn bottomShadow btnct btnct-nunsa"
+                      : "btnct btn  btn-secondary"
+                  }
+                  disabled={opinion.length > 0 ? false : true}
                 >
                   POST
                 </button>
@@ -58,23 +62,20 @@ const CreatePostSmall = () => {
             </div>
           </div>
         </div>
-        <div className={`${classes.inputSection} container mt-3`}>
-          <input
-            type="text"
+        <div className={`${classes.inputSection}  mt-4`}>
+          <textarea
+            placeholder="What's on your mind?"
+            // onBlur={() => {
+            //   setStudentOpinion("");
+            //   document.getElementById(`IdOfCommentArea`).value = "";
+            // }}
+            className={`form-control ${classes.postInput}`}
+            autoComplete="off"
+            autoCorrect="off"
+            aria-describedby="regimeDescriptionHelp"
             onChange={(e) => setOpinion(e.target.value)}
-            ref={hiddenFileInput}
-            className={classes.invisiput}
-            // style={{ display: "none" }}
+            // required
           />
-          <div
-            className={`blogText ${classes.defaultInputText}`}
-            onClick={handleClick}
-          >
-            <p>
-              {opinion}
-              <span className={`${classes.blinker} blogText`}>I</span>
-            </p>
-          </div>
         </div>
         {/* <div className="fixed-bottom">{opinion}</div> */}
       </div>
