0e29d2f8b5b85578cda59b7de8310753c9d52234
Edidiong Obodom <edijay17@gmail.com>
2023-08-04 12:36:17 +0100

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import FullLoader from "./components/fullLoader/FullLoader";
-const Homepage = lazy(() => import("./pages/Homepage"));
-const About = lazy(() => import("./pages/About"));
-const Blog = lazy(() => import("./pages/Blog"));
-const Executives = lazy(() => import("./pages/Executives"));
-const LoginView = lazy(() => import("./components/login/login"));
-const Signup = lazy(() => import("./components/signup/signup"));
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Executives from "./pages/Executives";
+import LoginView from "./components/login/login";
+import Signup from "./components/signup/signup";
+import CreatePostMain from "./components/createPost/createPostMain";
 const Dashboard = lazy(() => import("./pages/auth/Dashboard"));
 const Class = lazy(() => import("./pages/auth/Class"));
-const CreatePostMain = lazy(() =>
-  import("./components/createPost/createPostMain")
-);
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <Suspense fallback={<FullLoader />}>
-            <Homepage />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/about"
-        element={
-          <Suspense fallback={<FullLoader />}>
-            <About />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/blog"
-        element={
-          <Suspense fallback={<FullLoader />}>
-            <Blog />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/executives"
-        element={
-          <Suspense fallback={<FullLoader />}>
-            <Executives />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <Suspense fallback={<FullLoader />}>
-            <LoginView />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/signup"
-        element={
-          <Suspense fallback={<FullLoader />}>
-            <Signup />
-          </Suspense>
-        }
-      />
+      
+      <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/executives" element={<Executives />} />
+      <Route path="/login" element={<LoginView />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/student/create_post" element={<CreatePostMain />} />
+
+      {/* lazy loaded routes */}
       <Route
         path="/student/dashboard"
         element={
@@ -77,14 +37,6 @@ function App() {
         element={
           <Suspense fallback={<FullLoader />}>
             <Class />
-          </Suspense>
-        }
-      />
-      <Route
-        path="/student/create_post"
-        element={
-          <Suspense fallback={<FullLoader />}>
-            <CreatePostMain />
           </Suspense>
         }
       />
